ab4dbbba8fb2894e955d2e5fe4c6b3c0e720d17e
VictorAlvaradoAraujo <viosalva@espol.edu.ec>
2021-06-01 17:36:48 -0500

    mod

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,15 @@ import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
     return (
-        <footer  className="    bg-light text-dark-50">
+        <footer  className="row bg-light text-dark-50">
             <div>
                 <div className="row justify-content-center " >
                 
-                        <small  className="col-md-2 col-8 my-2 justify-content-center">Copyright &copy; Service - Soft Developer</small>
-                        
+                        <small  className="col-md-2 col-7 my-2 justify-content-center">Copyright &copy; Service - Soft Developer</small>
+                     
                   
                         <div className=" row justify-content-md-center">
+                                <hr></hr>
                                 <div className="row justify-content-md-center col-sm-2 col-6 mx-auto my-2 align-self-center" >
                                 <div  className="row col  ">
                                 <SocialIcon url="https://www.facebook.com/riosymontanas.ec" />
