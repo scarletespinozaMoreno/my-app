b1468ecbf341ff0ffb68e9819fc6881f4cdf7303
Scarlet Angelina Espinoza Moreno <scanespi@espol.edu.ec>
2021-06-08 22:41:28 -0500

    agregando codigo

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,14 @@ import app from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 const firebaseConfig={
-    apiKey: "AIzaSyBEiGPEfE9PFTzRbfQjWhP_KzpcRM3tXG0",
-    authDomain: "basehosteria.firebaseapp.com",
-    projectId: "basehosteria",
-    storageBucket: "basehosteria.appspot.com",
-    messagingSenderId: "710580182831",
-    appId: "1:710580182831:web:ba9ee8f61554622c1d4bda"
+    apiKey: "AIzaSyAdUEfnAG4sMYl5bN-iYwWRW7M07fBf2hk",
+    authDomain: "hosteria-95a60.firebaseapp.com",
+    databaseURL: "https://hosteria-95a60-default-rtdb.firebaseio.com",
+    projectId: "hosteria-95a60",
+    storageBucket: "hosteria-95a60.appspot.com",
+    messagingSenderId: "542004704893",
+    appId: "1:542004704893:web:e58870315ae52c054c6314",
+    measurementId: "G-80DEZ5YFN2"
 }
 //inicializar
 app.initializeApp(firebaseConfig);
